5e871c50a7fddf31cb9c9979b29a373bf8d7859d
Matteo Ratet <matteo.ratetvignat@emova.me>
2023-12-11 15:00:40 +0100

    Complete day 10 / Add day 11

--- solutions/2023/day_10/submissions.py
+++ solutions/2023/day_10/submissions.py
@@ -4,13 +4,17 @@ lines = open('input.txt').read().splitlines()
 # WRITE YOUR SOLUTION HERE
 
 next_move_allowed = {
-    (1, 0): "-7J",
-    (-1, 0): "-FL",
-    (0, -1): "|F7",  # The maze is reversed on the y-axis
-    (0, 1): "|LJ"
+    "S": [(1, 0), (0, 1)], # [(0, 1), (0, -1), (1, 0), (-1, 0)] we use a F to debug here
+    "|": [(0, 1), (0, -1)],
+    "-": [(-1, 0), (1, 0)],
+    "J": [(-1, 0), (0, -1)],
+    "F": [(1, 0), (0, 1)],
+    "7": [(0, 1), (-1, 0)],
+    "L": [(0, -1), (1, 0)],
 }
 
 def part_1(lines):
+
     maze = {
         (x, y): c
         for y, line in enumerate(lines)
@@ -18,26 +22,67 @@ def part_1(lines):
     }
 
     s_co = next(co for co, v in maze.items() if v == 'S')
-    print(s_co)
     q = [s_co]
-    visited = []
+    visited = set()
 
     while q:
         x, y = q.pop()
-        visited.append((x, y))
-        for move, char_allowed in next_move_allowed.items():
+        current_car = maze.get((x, y))
+        visited.add((x, y))
+
+        for move in next_move_allowed[current_car]:
             nx = x + move[0]
             ny = y + move[1]
-            next_move = maze.get((nx, ny), ".")
 
-            if next_move in char_allowed and (nx, ny) not in visited:
+            if (nx, ny) not in visited:
                 q.append((nx, ny))
 
-    return (len(visited) - 1) // 2
+    return len(visited) / 2
 
 
 def part_2(lines):
-    return 0
+
+    maze = {
+        (x, y): c
+        for y, line in enumerate(lines)
+        for x, c in enumerate(line)
+    }
+
+    s_co = next(co for co, v in maze.items() if v == 'S')
+    q = [s_co]
+    visited = set()
+
+    while q:
+        x, y = q.pop()
+        current_car = maze.get((x, y))
+        visited.add((x, y))
+
+        for move in next_move_allowed[current_car]:
+            nx = x + move[0]
+            ny = y + move[1]
+
+            if (nx, ny) not in visited:
+                q.append((nx, ny))
+
+    area = 0
+    n, m = len(lines), len(lines[0])
+    for y in range(n):
+        border_count = 0
+        for x in range(m):
+            # From a horizontal analysis, we can choose either LJ| or F7|
+            # I took LJ| because my starting tile is F
+            border_flag = (x, y) in visited
+            if border_flag and maze[(x, y)] in "LJ|":
+                border_count += 1
+
+            elif not border_flag and border_count % 2 == 1:
+                area += 1
+
+    return area
+
+
+
+
 # END OF SOLUTION
 
 
---
+++ solutions/2023/day_11/submissions.py
@@ -0,0 +1,76 @@
+
+lines = open('input.txt').read().splitlines()
+
+# WRITE YOUR SOLUTION HERE
+
+
+def galaxie_distance(g1, g2, row_expands, col_expands, expand):
+    x1, y1 = g1
+    x2, y2 = g2
+    x_max, x_min = max(x1, x2), min(x1, x2)
+    y_max, y_min = max(y1, y2), min(y1, y2)
+
+    row_expansion = len([1 for y in row_expands if y_min < y < y_max])
+    col_expansion = len([1 for x in col_expands if x_min < x < x_max])
+
+    return abs(x_max - x_min + (expand - 1) * row_expansion) + abs(y_max - y_min + (expand - 1) * col_expansion)
+
+
+def part_1(lines):
+
+    galaxies = [(x, y)
+                for y, line in enumerate(lines)
+                for x, c in enumerate(line)
+                if c == "#"]
+
+    n, m = len(lines), len(lines[0])
+    empty_row = list(set(range(m)) - set([g[1] for g in galaxies]))
+    empty_col = list(set(range(n)) - set([g[0] for g in galaxies]))
+
+    expansion = 2
+
+    ans = 0
+
+    for gi in galaxies:
+        for gj in galaxies:
+            ans += galaxie_distance(gi, gj, empty_row, empty_col, expansion)
+
+    return ans // 2
+
+def part_2(lines):
+
+    galaxies = [(x, y)
+                for y, line in enumerate(lines)
+                for x, c in enumerate(line)
+                if c == "#"]
+
+    n, m = len(lines), len(lines[0])
+    empty_row = list(set(range(m)) - set([g[1] for g in galaxies]))
+    empty_col = list(set(range(n)) - set([g[0] for g in galaxies]))
+
+    expansion = 1e6
+
+    ans = 0
+
+    for gi in galaxies:
+        for gj in galaxies:
+            ans += galaxie_distance(gi, gj, empty_row, empty_col, expansion)
+
+    return ans // 2
+# END OF SOLUTION
+
+
+test_input = open('input-test.txt').read().splitlines()
+test_lines = []
+for i, line in enumerate(test_input[3:]):
+    if line[0] == '-':
+        break
+    test_lines.append(line)
+solution = test_input[i + 4]
+
+print(f'My answer on test set for the first problem is {part_1(test_lines)}')
+print(solution)
+print(f'My answer is {part_1(lines)}')
+
+print(f'My answer on test set for the second problem is {part_2(test_lines)}')
+print(f'My answer is {part_2(lines)}')
