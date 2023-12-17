801be1d3cded914c893fd735e1d75eb3ca5bce78
mratet <mratet.vignat@gmail.com>
2023-12-17 14:22:54 +0100

    Add Day 15 and Day 17

---
+++ solutions/2023/day_15/submissions.py
@@ -0,0 +1,61 @@
+import collections
+
+lines = open('input.txt').read().splitlines()
+
+# WRITE YOUR SOLUTION HERE
+
+def hash(string):
+    value = 0
+    for c in string:
+        value = ((value + ord(c)) * 17) % 256
+    return value
+
+def part_1(lines):
+    sequence = lines[0].split(',')
+    ans = 0
+    for u in sequence:
+        ans += hash(u)
+    return ans
+
+def part_2(lines):
+    sequence = lines[0].split(',')
+    boxes = collections.defaultdict(list)
+    for u in sequence:
+        if '-' in u:
+            label = u[:-1]
+            box_number = hash(label)
+
+            for i, (lab, _) in enumerate(boxes[box_number]):
+                if lab == label:
+                    boxes[box_number].pop(i)
+        else:
+            label, focal_length = u.split('=')
+            box_number = int(hash(label))
+            index = [i for i, (lab, _) in enumerate(boxes[box_number]) if label == lab]
+            if index:
+                boxes[box_number][index[0]] = (label, int(focal_length))
+            else:
+                boxes[box_number].append((label, int(focal_length)))
+
+    ans = 0
+    for box_number in boxes.keys():
+        for i, (l, focal_length) in enumerate(boxes[box_number]):
+            ans += (box_number + 1) * (i + 1) * focal_length
+    return ans
+# END OF SOLUTION
+
+
+test_input = open('input-test.txt').read().splitlines()
+test_lines = []
+for i, line in enumerate(test_input[3:]):
+    if line[0] == '-':
+        break
+    test_lines.append(line)
+solution = test_input[i + 4]
+
+print(f'My answer on test set for the first problem is {part_1(test_lines)}')
+print(solution)
+print(f'My answer is {part_1(lines)}')
+
+print(f'My answer on test set for the second problem is {part_2(test_lines)}')
+print(f'My answer is {part_2(lines)}')
---
+++ solutions/2023/day_17/submissions.py
@@ -0,0 +1,72 @@
+
+lines = open('input.txt').read().splitlines()
+
+# WRITE YOUR SOLUTION HERE
+
+from heapq import heappop, heappush
+
+N, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0)
+
+
+def dijkstra(graph, source, target, part):
+
+    # Little bug here, try either S or E
+    heap = [(0, source, E, -1)]
+    visited = {}
+
+    while heap:
+        dist, node, dir_, cnt_dir_ = heappop(heap)
+        if (node, dir_, cnt_dir_) in visited:
+            continue
+        visited[(node, dir_, cnt_dir_)] = dist
+
+        for new_dir in ([N, S, W, E]):
+            next_node = (node[0] + new_dir[0], node[1] + new_dir[1])
+            new_cnt_dir = (1 if new_dir != dir_ else cnt_dir_ + 1)
+            isnt_reverse = (dir_[0] * new_dir[0] + dir_[1] * new_dir[1] != -1)
+
+            match part:
+                case 'part_1': isvalid = (new_cnt_dir <= 3)
+                case 'part_2': isvalid = (new_cnt_dir <= 10 and (new_dir == dir_ or cnt_dir_ >= 4 or cnt_dir_ == -1))
+
+            if next_node in graph and isnt_reverse and isvalid:
+                cost = graph[next_node]
+                heappush(heap, (dist + cost, next_node, new_dir, new_cnt_dir))
+
+    return min([v for (node, _, _), v in visited.items() if node == target])
+
+def _parse(input):
+    graph = {(x, y): int(c)
+             for y, line in enumerate(input)
+             for x, c in enumerate(line)}
+    return graph
+
+
+def part_1(input):
+    graph = _parse(input)
+    n, m = len(input), len(input[0])
+
+    return dijkstra(graph, (0, 0), (m - 1, n - 1), 'part_1')
+
+def part_2(input):
+    graph = _parse(input)
+    n, m = len(input), len(input[0])
+
+    return dijkstra(graph, (0, 0), (m - 1, n - 1), 'part_2')
+# END OF SOLUTION
+
+
+test_input = open('input-test.txt').read().splitlines()
+test_lines = []
+for i, line in enumerate(test_input[3:]):
+    if line[0] == '-':
+        break
+    test_lines.append(line)
+solution = test_input[i + 4]
+
+print(f'My answer on test set for the first problem is {part_1(test_lines)}')
+print(solution)
+print(f'My answer is {part_1(lines)}')
+
+print(f'My answer on test set for the second problem is {part_2(test_lines)}')
+print(f'My answer is {part_2(lines)}')
