836159137acb983e1614639a973c926d5c2e4032
mratet <mratet.vignat@gmail.com>
2024-01-09 21:56:03 +0100

    Add day 5, 6 2016

---
+++ solutions/2016/day_06.py
@@ -0,0 +1,23 @@
+import itertools, re, collections
+from aocd import get_data
+input = get_data(day=6, year=2016).splitlines()
+
+def send_message(input, part='part_1'):
+    col = zip(*input)
+    t = ''
+    for row in col:
+        count = collections.Counter(row)
+        c, _ = max(count.items(), key=lambda x: x[1])
+        if part == 'part_2':
+            c, _ = min(count.items(), key=lambda x: x[1])
+        t += c
+    return t
+
+def part_1(input):
+    return send_message(input, 'part_1')
+
+def part_2(input):
+    return send_message(input, 'part_2')
+
+print(f'My answer is {part_1(input)}')
+print(f'My answer is {part_2(input)}')
