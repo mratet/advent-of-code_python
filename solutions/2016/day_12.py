fd3f136cf08480c3a0f3d1b82db828d511301895
mratet <mratet.vignat@gmail.com>
2024-01-12 09:59:11 +0100

    Add day 12, 13 2016

---
+++ solutions/2016/day_12.py
@@ -0,0 +1,43 @@
+import itertools, re, collections
+from aocd import get_data
+input = get_data(day=12, year=2016).splitlines()
+
+def program_execution(registers, input):
+
+    line_index = 0
+    n = len(input)
+
+    while line_index < n:
+        instructions, *args = input[line_index].split()
+        match instructions:
+            case 'inc':
+                c = args[0]
+                registers[c] = registers[c] + 1
+                line_index += 1
+            case 'dec':
+                c = args[0]
+                registers[c] = registers[c] - 1
+                line_index += 1
+            case 'cpy':
+                x, y = args[0], args[1]
+                registers[y] = registers[x] if x in 'abcd' else int(x)
+                line_index += 1
+            case 'jnz':
+                x, y = args[0], args[1]
+                cond = registers[x] if x in 'abcd' else int(x)
+                line_index += int(y) if cond != 0 else 1
+
+    return registers
+
+def part_1(input):
+    registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+    registers = program_execution(registers, input)
+    return registers['a']
+
+def part_2(input):
+    registers = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
+    registers = program_execution(registers, input)
+    return registers['a']
+
+print(f'My answer is {part_1(input)}')
+print(f'My answer is {part_2(input)}')
---
+++ solutions/2016/day_13.py
@@ -0,0 +1,40 @@
+import itertools, re, collections
+from aocd import get_data
+input = int(get_data(day=13, year=2016))
+
+def is_a_wall(coord, fav_num):
+    x, y = coord
+    i = x * x + 3 * x + 2 * x * y + y + y * y + fav_num
+    return i.bit_count() % 2 == 1
+
+def solve(input, part='part_1'):
+    start = (1, 1)
+    q = collections.deque()
+    q.append(start)
+    visited = set()
+    cnt = 0
+
+    while q:
+        for _ in range(len(q)):
+            x, y = q.popleft()
+            if (x, y) in visited: continue
+            visited.add((x, y))
+            if part=='part_1' and (x, y) == (31, 39): return cnt
+
+            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                nx, ny = x + dx, y + dy
+
+                if 0 <= nx and 0 <= ny and not is_a_wall((nx, ny), input):
+                    q.append((nx, ny))
+        cnt += 1
+        if part == 'part_2' and cnt == 51:
+            return len(visited)
+
+def part_1(input):
+    return solve(input, 'part_1')
+
+def part_2(input):
+    return solve(input, 'part_2')
+
+print(f'My answer is {part_1(input)}')
+print(f'My answer is {part_2(input)}')
