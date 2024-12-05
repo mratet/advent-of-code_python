0e570972272622ff5990857d8aa250fbc9fefbb4
mratet <mratet.vignat@gmail.com>
2024-12-05 22:37:42 +0100

    AOC 2024 - 5 clean ver

--- solutions/2024/day_05.py
+++ solutions/2024/day_05.py
@@ -1,55 +1,28 @@
 from aocd import get_data, submit
-input = get_data(day=5, year=2024).splitlines()
+input = get_data(day=5, year=2024).split('\n\n')
 from collections import defaultdict
+from functools import cmp_to_key
 
 # WRITE YOUR SOLUTION HERE
 def parse_input(lines):
     dict_rule = defaultdict(list)
-    pages = []
-    c = False
-    for line in lines:
-        if not line:
-            c = True
-            continue
-        if not c:
-            a, b = line.split('|')
-            dict_rule[int(a)].append(int(b))
-        else:
-            pages.append([int(n) for n in line.split(',')])
-    return dict_rule, pages
+    for line in lines[0].split():
+        a, b = map(int, line.split('|'))
+        dict_rule[a].append(b)
 
-def is_valid_pages(dict_rule, pages):
-    for i, p in enumerate(pages):
-        if any([next_page in pages[:i] for next_page in dict_rule[p]]):
-            return False
-    return True
+    pages = [list(map(int, line.split(','))) for line in lines[1].split()]
+    return dict_rule, pages
 
 def part_1(lines):
     dict_rule, pages = parse_input(lines)
-    return sum([page[len(page) // 2] for page in pages if is_valid_pages(dict_rule, page)])
-
-def correct_1_error(page, dict_rules):
-    for idx, p in enumerate(page):
-        if any([next_page in page[:idx] for next_page in dict_rules[p]]):
-            break
-    idx -= 1
-
-    while idx < len(page) and not is_valid_pages(dict_rules, page):
-        wrong_page = page.pop(idx)
-        page.insert(idx + 1, wrong_page)
-        idx += 1
-    return page
+    cmp = lambda a, b : -1 * int(b in dict_rule[a])
+    return sum([page[len(page) // 2] for page in pages if (page == sorted(page, key=cmp_to_key(cmp)))])
 
 def part_2(lines):
     dict_rule, pages = parse_input(lines)
+    cmp = lambda a, b : -1 * int(b in dict_rule[a])
+    return sum([sorted_p[len(sorted_p) // 2] for page in pages if (sorted_p := sorted(page, key=cmp_to_key(cmp))) != page])
 
-    cnt = 0
-    for p in pages:
-        if not is_valid_pages(dict_rule, p):
-            while not is_valid_pages(dict_rule, p):
-                p = correct_1_error(p, dict_rule)
-            cnt += p[len(p) // 2]
-    return cnt
 # END OF SOLUTION
 print(f'My answer is {part_1(input)}')
 print(f'My answer is {part_2(input)}')
