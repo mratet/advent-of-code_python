9c8466a15f12b9c64fcb9b516ac53e0ea7007777
mratet <mratet.vignat@gmail.com>
2024-12-07 16:51:23 +0100

    AOC 2024 - 6, 7 clean ver

--- solutions/2024/day_06.py
+++ solutions/2024/day_06.py
@@ -1,11 +1,12 @@
+from collections import defaultdict
 from aocd import get_data
 input = get_data(day=6, year=2024).splitlines()
 
 # WRITE YOUR SOLUTION HERE
 def get_guard_path(obstacles, start, lines):
-    path = {start}
+    path = defaultdict(list)
+    path[start].append(0)
     gi, gj = start
-    par_path = {(gi, gj, 0)}
     direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     idx = 0
     n, m = len(lines), len(lines[0])
@@ -14,44 +15,40 @@ def get_guard_path(obstacles, start, lines):
         ni, nj = gi + di, gj + dj
         if (ni, nj) not in obstacles:
             gi, gj = ni, nj
-            path.add((gi, gj))
-            if (gi, gj, idx) in par_path:
+            if idx in path.get((gi, gj), []):
                 return False
-            else:
-                par_path.add((gi, gj, idx))
+            path[(gi, gj)].append(idx)
         else:
             idx = (idx + 1) % 4
     return path
 
-def part_1(lines):
+def parse_input(lines):
     obstacles = set()
+    start = None
     for i, line in enumerate(lines):
         for j, c in enumerate(line):
             if c == "#":
                 obstacles.add((i, j))
             elif c == '^':
-                gi, gj = i, j
-    path = get_guard_path(obstacles, (gi, gj), lines)
+                start = (i, j)
+    return obstacles, start
+
+def part_1(lines):
+    obstacles, start = parse_input(lines)
+    path = get_guard_path(obstacles, start, lines)
     return len(path) - 1
 
 def part_2(lines):
-    obstacles = set()
-    for i, line in enumerate(lines):
-        for j, c in enumerate(line):
-            if c == "#":
-                obstacles.add((i, j))
-            elif c == '^':
-                gi, gj = i, j
-
+    obstacles, start = parse_input(lines)
     cnt = 0
-    path = get_guard_path(obstacles, (gi, gj), lines)
+    path = get_guard_path(obstacles, start, lines)
     for pos in path:
         obstacles.add(pos)
-        if not get_guard_path(obstacles, (gi, gj), lines):
+        if not get_guard_path(obstacles, start, lines):
             cnt += 1
         obstacles.remove(pos)
-
     return cnt
+
 # END OF SOLUTION
 print(f'My answer is {part_1(input)}')
 print(f'My answer is {part_2(input)}')
