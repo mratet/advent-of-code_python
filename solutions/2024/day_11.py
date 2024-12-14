bdda55e23391ac9822d2e862d6f5bd149948fc09
mratet <mratet.vignat@gmail.com>
2024-12-14 17:57:52 +0100

    AOC 2024 - 11 add alternatives method

--- solutions/2024/day_11.py
+++ solutions/2024/day_11.py
@@ -1,44 +1,68 @@
 from aocd import get_data, submit
 input = get_data(day=11, year=2024)
 from functools import lru_cache
+from math import log
+from collections import Counter, defaultdict
 
 # WRITE YOUR SOLUTION HERE
-def get_next_state(stones):
-    ### Naive approach
-    new_stones = []
-    for i in range(len(stones)):
-        if stones[i] == 0:
-            new_stones.append(1)
-        elif len(str(stones[i])) % 2 == 0:
-            idx =len(str(stones[i])) // 2
-            val1, val2 = str(stones[i])[:idx], str(stones[i])[idx:]
-            new_stones.append(int(val1))
-            new_stones.append(int(val2))
-        else:
-            new_stones.append(stones[i] * 2024)
-    return new_stones
-
-def part_1(lines):
-    stones = [int(n) for n in lines.split()]
-    for _ in range(25):
-        stones = get_next_state(stones)
+def naive_approach(stones, n):
+    assert n < 30
+    for _ in range(n):
+        new_stones = []
+        for i, stone in enumerate(stones):
+            if stone == 0:
+                new_stones.append(1)
+            elif len(str(stone)) % 2 == 0:
+                mid = len(str(stone)) // 2
+                v1, v2 = int(str(stone)[:mid]), int(str(stone)[mid:])
+                new_stones.append(v1)
+                new_stones.append(v2)
+            else:
+                new_stones.append(stone * 2024)
+        stones = new_stones
     return len(stones)
+
+def dict_approach(stones_dict, n):
+    stones_dict = Counter(stones_dict)
+    for _ in range(n):
+        new_stones = defaultdict(int)
+        for stone, count in stones_dict.items():
+            if stone == 0:
+                new_stones[1] += count
+                continue
+            cnt_bits = int(log(stone, 10)) + 1
+            if cnt_bits % 2 == 0:
+                middle = 10 ** (cnt_bits // 2)
+                new_stones[stone // middle] += count
+                new_stones[stone % middle] += count
+            else:
+                new_stones[stone * 2024] += count
+        stones_dict = new_stones.copy()
+    return sum(stones_dict.values())
 
 @lru_cache(maxsize=None)
 def get_stone_size(stone, i):
-    if i == 75:
+    if i == 0:
         return 1
-    if stone == '0':
-        return get_stone_size('1', i + 1)
-    elif len(stone) % 2 == 0:
-        idx = len(stone) // 2
-        return get_stone_size(stone[:idx], i + 1) + get_stone_size(str(int(stone[idx:])), i + 1)
+    if stone == 0:
+        return get_stone_size(1, i - 1)
+    cnt_bits = int(log(stone, 10)) + 1
+    if cnt_bits % 2 == 0:
+        middle = 10 ** (cnt_bits // 2)
+        return get_stone_size(stone // middle, i - 1) + get_stone_size(stone % middle, i - 1)
     else:
-        return get_stone_size(str(int(stone) * 2024), i + 1)
+        return get_stone_size(stone * 2024, i - 1)
+
+def part_1(lines):
+    stones = [int(s) for s in lines.split()]
+    # return naive_approach(stones, 25)
+    return dict_approach(stones, 25)
+    # return sum([get_stone_size(x, 25) for x in stones])
 
 def part_2(lines):
-    stones = [s for s in lines.split()]
-    return sum([get_stone_size(x, 0) for x in stones])
+    stones = [int(s) for s in lines.split()]
+    return dict_approach(stones, 75)
+    # return sum([get_stone_size(x, 75) for x in stones])
 
 # END OF SOLUTION
 print(f'My answer is {part_1(input)}')
