6bb2a2e126aa255bdacf17fb4aa28a5efabe8cdd
mratet <mratet.vignat@gmail.com>
2024-12-18 19:23:52 +0100

    AOC 2024 - 18 clean ver

--- solutions/2024/day_18.py
+++ solutions/2024/day_18.py
@@ -1,11 +1,12 @@
-from collections import defaultdict
-
 from aocd import get_data, submit
 input = get_data(day=18, year=2024).splitlines()
+from collections import defaultdict
 from heapq import heappush, heappop
 import re
 
 # WRITE YOUR SOLUTION HERE
+X, Y = 71, 71
+
 def dijkstra(graph, source, target=None):
     prec = {k: None for k in graph}
     black = {k: False for k in graph}
@@ -24,36 +25,30 @@ def dijkstra(graph, source, target=None):
                     dist[neighbor] = dist_neighbor
                     prec[neighbor] = node
                     heappush(heap, (dist_neighbor, neighbor))
-
     return dist, prec
 
 def construct_grid(lines, i_max):
-    X, Y = 71, 71
     grid = [['.' for _ in range(X)] for _ in range(Y)]
-    for i in range(i_max):
-        y, x = map(int, re.findall(r'-?\d+', lines[i]))
+    for line in lines[:i_max]:
+        y, x = map(int, re.findall(r'-?\d+', line))
         grid[x][y] = '#'
     graph = defaultdict(list)
     for x in range(X):
         for y in range(Y):
             c = grid[x][y]
-            if c == '.':
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < X and 0 <= ny < Y and grid[nx][ny] != '#':
-                        graph[(x, y)].append((nx, ny))
+            if c != '.': continue
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < X and 0 <= ny < Y and grid[nx][ny] != '#':
+                    graph[(x, y)].append((nx, ny))
     return graph
 
 def part_1(lines):
-    X = 71
-    Y = 71
     graph = construct_grid(lines, 1024)
     dist, prec = dijkstra(graph, (0, 0), (X - 1, Y - 1))
     return dist[(X - 1, Y - 1)]
 
 def part_2(lines):
-    X = 71
-    Y = 71
     l, r = 0, len(lines) - 1
     while l <= r:
         m = (l + r) // 2
