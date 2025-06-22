fd870f7183f1308f3ac960857615056f41268f21
mratet <mratet.vignat@gmail.com>
2025-06-22 15:28:51 +0200

    AOC 2023 - 22 clean ver

--- solutions/2023/day_22/clean_solution.py
+++ solutions/2023/day_22/clean_solution.py
@@ -2,118 +2,104 @@ from aocd import get_data
 
 input = get_data(day=22, year=2023).splitlines()
 
-# WRITE YOUR SOLUTION HERE
-from collections import deque
+from collections import deque, defaultdict
 
 
-def _parse(input):
+def parse_input(lines):
     bricks = []
-    for line in input:
+    for line in lines:
         start, end = line.split("~")
         start = [int(c) for c in start.split(",")]
         end = [int(c) for c in end.split(",")]
         bricks.append([start, end])
-
     return bricks
 
 
-def support_graph(bricks):
+def get_is_supported_by_dict(bricks):
+    bricks = sorted(bricks, key=lambda brick: brick[0][2])
     len_basis = 10
     basis = {(x, y): (0, -1) for x in range(len_basis) for y in range(len_basis)}
 
-    is_supported_by = [[] for _ in range(len(bricks) + 1)]
+    is_supported_by = [[] for _ in range(len(bricks))]
 
     for i, [(x_s, y_s, z_s), (x_e, y_e, z_e)] in enumerate(bricks):
-        if x_s == x_e and y_s == y_e:
-            is_supported_by[i + 1].append(basis[(x_e, y_e)][1])
-            basis[(x_s, y_s)] = (basis[(x_s, y_s)][0] + z_e - z_s + 1, i)
-
-        elif x_s == x_e:
-            z_max = max([basis[(x_e, y)][0] for y in range(y_s, y_e + 1)])
-            for y in range(y_s, y_e + 1):
-                if (
-                    basis[(x_e, y)][0] == z_max
-                    and basis[(x_e, y)][1] not in is_supported_by[i + 1]
-                ):
-                    is_supported_by[i + 1].append(basis[(x_e, y)][1])
-                basis[(x_e, y)] = (z_max + 1, i)
-
-        elif y_s == y_e:
+        if x_s != x_e:
             z_max = max([basis[(x, y_e)][0] for x in range(x_s, x_e + 1)])
             for x in range(x_s, x_e + 1):
-                if (
-                    basis[(x, y_e)][0] == z_max
-                    and basis[(x, y_e)][1] not in is_supported_by[i + 1]
-                ):
-                    is_supported_by[i + 1].append(basis[(x, y_e)][1])
+                curr_z, base_id = basis[(x, y_e)]
+                if curr_z == z_max and base_id not in is_supported_by[i]:
+                    is_supported_by[i].append(base_id)
                 basis[(x, y_e)] = (z_max + 1, i)
-        else:
-            print("There are square in the input !")
+        elif y_s != y_e:
+            z_max = max([basis[(x_e, y)][0] for y in range(y_s, y_e + 1)])
+            for y in range(y_s, y_e + 1):
+                curr_z, base_id = basis[(x_e, y)]
+                if curr_z == z_max and base_id not in is_supported_by[i]:
+                    is_supported_by[i].append(base_id)
+                basis[(x_e, y)] = (z_max + 1, i)
+        else:  # z_s != z_e
+            is_supported_by[i].append(basis[(x_e, y_e)][1])
+            curr_z, _ = basis[(x_s, y_s)]
+            basis[(x_s, y_s)] = (curr_z + z_e - z_s + 1, i)
 
     return is_supported_by
 
 
-def part_1(lines):
-    bricks = _parse(lines)
-    bricks.sort(key=lambda x: x[0][2])
-
-    is_supported_by = support_graph(bricks)
-
-    not_safe = [False for _ in range(len(bricks))]
-
+def get_unsafe_brick(is_supported_by):
+    not_safe = [False for _ in range(len(is_supported_by))]
     for tab in is_supported_by:
         if len(tab) == 1 and tab[0] != -1:
             not_safe[tab[0]] = True
+    return not_safe
 
-    return len(bricks) - sum(not_safe)
+
+def compute_support(is_supported_by):
+    support = defaultdict(list)
+    for i, tab in enumerate(is_supported_by):
+        for neighboor in tab:
+            if neighboor != -1:
+                support[neighboor].append(i)
+    return support
 
 
-def dfs(start, graph):
-    visited = set()
+def fallen_bricks(start, support, is_supported_by):
+    fallen_bricks = set()
     q = deque()
     q.append(start)
 
     while q:
-        node = q.popleft()
-        visited.add(node)
+        brick = q.popleft()
+        fallen_bricks.add(brick)
 
-        for neighboor in graph[node]:
-            if neighboor not in visited:
-                q.append(neighboor)
+        for brick_on in support[brick]:
+            if brick_on in fallen_bricks:
+                continue
+            if all(b in fallen_bricks for b in is_supported_by[brick_on]):
+                q.append(brick_on)
 
-    return len(visited)
+    return len(fallen_bricks) - 1
+
+
+# WRITE YOUR SOLUTION HERE
+def part_1(lines):
+    bricks = parse_input(lines)
+    is_supported_by = get_is_supported_by_dict(bricks)
+    not_safe = get_unsafe_brick(is_supported_by)
+    return len(bricks) - sum(not_safe)
 
 
 def part_2(input):
-    bricks = _parse(input)
-    bricks.sort(key=lambda x: x[0][2])
-
-    is_supported_by = support_graph(bricks)
-
-    not_safe = [False for _ in range(len(bricks))]
-    print(is_supported_by)
-
-    support = [[] for _ in range(len(bricks) + 1)]
-    for i, tab in enumerate(is_supported_by):
-        for neighboor in tab:
-            support[neighboor + 1].append(i - 1)
-
-    for tab in is_supported_by:
-        if len(tab) == 1 and tab[0] != -1:
-            not_safe[tab[0]] = True
-    ans = 0
-    print(support)
-    print(not_safe)
-    for i in range(len(not_safe)):
-        if not not_safe[i]:
-            ans += dfs(i + 1, support)
-    print(dfs(0, support))
-
-    return ans
+    bricks = parse_input(input)
+    is_supported_by = get_is_supported_by_dict(bricks)
+    not_safe = get_unsafe_brick(is_supported_by)
+    support = compute_support(is_supported_by)
+    return sum(
+        fallen_bricks(i, support, is_supported_by)
+        for i, not_safe_bool in enumerate(not_safe)
+        if not_safe_bool
+    )
 
 
 # END OF SOLUTION
-
-
 print(f"My answer is {part_1(input)}")
 print(f"My answer is {part_2(input)}")
