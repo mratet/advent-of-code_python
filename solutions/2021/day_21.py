466ddbb9dd71eb44d335237986d0d1cb9817701c
mratet <mratet.vignat@gmail.com>
2025-08-18 20:05:14 +0200

    AOC 2021 - 21 clean ver

--- solutions/2021/day_21.py
+++ solutions/2021/day_21.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from aocd import get_data
 import re
 from itertools import product, cycle, islice
@@ -12,19 +14,47 @@ class Player:
         self.score = 0
 
 
-GOAL_SCORE = 1000
 GAMEBOARD_SIZE = 10
-SIZE = 10
+
+
+def _parse_input(lines):
+    return [int(re.findall(r"(\d+)$", line)[0]) - 1 for line in lines]
 
 
 def single_player_game(player, dice_gen):
     turn = []
-    while player.score < GOAL_SCORE:
+    while player.score < 1000:
         dice = next(dice_gen)
         player.position = (player.position + dice) % GAMEBOARD_SIZE
         player.score += player.position + 1
         turn.append(player.score)
     return turn
+
+
+@lru_cache(maxsize=None)
+def play_quantum_game(p1_position, p1_score, p2_position, p2_score, turn):
+    if p1_score >= 21:
+        return (1, 0)
+    if p2_score >= 21:
+        return (0, 1)
+
+    total_wins = (0, 0)
+    for die in product([1, 2, 3], repeat=3):
+        roll_sum = sum(die)
+        if turn == 0:
+            new_p1_position = (p1_position + roll_sum) % GAMEBOARD_SIZE
+            new_p1_score = p1_score + new_p1_position + 1
+            child_wins = play_quantum_game(
+                new_p1_position, new_p1_score, p2_position, p2_score, (turn + 1) % 2
+            )
+        if turn == 1:
+            new_p2_position = (p2_position + roll_sum) % GAMEBOARD_SIZE
+            new_p2_score = p2_score + new_p2_position + 1
+            child_wins = play_quantum_game(
+                p1_position, p1_score, new_p2_position, new_p2_score, (turn + 1) % 2
+            )
+        total_wins = total_wins[0] + child_wins[0], total_wins[1] + child_wins[1]
+    return total_wins
 
 
 # WRITE YOUR SOLUTION HERE
@@ -39,10 +69,8 @@ def part_1(lines):
     def odd_index(gen):
         return islice(gen, 1, None, 2)
 
-    pattern = r"Player \d starting position: (\d+)"
-
-    p1 = Player(int(re.findall(pattern, lines[0])[0]) - 1)
-    p2 = Player(int(re.findall(pattern, lines[1])[0]) - 1)
+    p1_position, p2_position = _parse_input(lines)
+    p1, p2 = Player(p1_position), Player(p2_position)
 
     p1_gen = even_index(sum_by_three(cycle(range(1, 101))))
     p2_gen = odd_index(sum_by_three(cycle(range(1, 101))))
@@ -57,33 +85,10 @@ def part_1(lines):
 
 
 def part_2(lines):
-    return
+    p1_position, p2_position = _parse_input(lines)
+    return max(play_quantum_game(p1_position, 0, p2_position, 0, 0))
 
 
 # END OF SOLUTION
 print(f"My answer is {part_1(input)}")
-# print(f'My answer is {part_2(input)}')
-
-# die_casting = list(product([1, 2, 3], repeat=3))
-# L = [ sum(d) for d in die_casting ]
-# 3 1
-# 4 3
-# 5 6
-# 6 7
-# 7 6
-# 8 3
-# 9 1
-# die_casting = product(range(3, 10), repeat=SIZE)
-# s = 0
-# for _ in die_casting:
-#     s += 1
-# print(s)
-# # n = len(die_casting)
-# s = 0
-# for d in die_casting:
-#     p = Player(9)
-#     try:
-#         t = single_player_game(p, iter(d))
-#     except StopIteration:
-#         s += 1
-# print(s)
+print(f"My answer is {part_2(input)}")
