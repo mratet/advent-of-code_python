cfddc7c6a3764c56696b68e97c2216cfd2d7f81f
mratet <mratet.vignat@gmail.com>
2025-08-19 21:00:33 +0200

    AOC 2021 - 23 clean ver

--- solutions/2018/day_23.py
+++ solutions/2018/day_23.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 import re
+from heapq import heappush, heappop
 
 input = get_data(day=23, year=2018).splitlines()
 
@@ -10,6 +11,51 @@ def manhattan_distance(p1, p2):
 
 def parse_input(lines):
     return [tuple(map(int, re.findall(r"-?\d+", line))) for line in lines]
+
+
+def divide_box(box):
+    if box[0] == box[1]: return [box]
+
+    (mx, my, mz), (Mx, My, Mz) = box
+    cx = (mx + Mx) // 2
+    cy = (my + My) // 2
+    cz = (mz + Mz) // 2
+    return [
+        ((mx, my, mz), (cx, cy, cz)),
+        ((cx + 1, my, mz), (Mx, cy, cz)),
+        ((mx, cy + 1, mz), (cx, My, cz)),
+        ((mx, my, cz + 1), (cx, cy, Mz)),
+        ((mx, cy + 1, cz + 1), (cx, My, Mz)),
+        ((cx + 1, my, cz + 1), (Mx, cy, Mz)),
+        ((cx + 1, cy + 1, mz), (Mx, My, cz)),
+        ((cx + 1, cy + 1, cz + 1), (Mx, My, Mz)),
+    ]
+
+
+def get_closest_point(nanobot, box):
+    nx, ny, nz = nanobot
+    (mx, my, mz), (Mx, My, Mz) = box
+    px = max(mx, min(nx, Mx))
+    py = max(my, min(ny, My))
+    pz = max(mz, min(nz, Mz))
+    return (px, py, pz)
+
+
+def count_intersection(box, nanobots):
+    box_score = 0
+    for nanobot in nanobots:
+        nx, ny, nz, nr = nanobot
+        closest_point = get_closest_point((nx, ny, nz), box)
+        dist = manhattan_distance(closest_point, nanobot)
+        if dist <= nr:
+            box_score += 1
+    return box_score
+
+
+def distance_to_origin(box):
+    origin = (0, 0, 0)
+    ox, oy, oz = get_closest_point(origin, box)
+    return abs(ox) + abs(oy) + abs(oz)
 
 
 # WRITE YOUR SOLUTION HERE
@@ -23,8 +69,27 @@ def part_1(lines):
 
 
 def part_2(lines):
-    # Have a look at Sweep Line
-    return
+    nanobots = parse_input(lines)
+    min_coord = min(c for bot in nanobots for c in bot[:3])
+    max_coord = max(c for bot in nanobots for c in bot[:3])
+    max_abs_coord = max(abs(min_coord), abs(max_coord))
+
+    size = 1
+    while size < max_abs_coord:
+        size *= 2
+
+    initial_box = ((-size, -size, -size), (size - 1, size - 1, size - 1))
+    heap = [(-len(nanobots), 0, initial_box)]
+
+    while True:
+        neg_score, dist, box = heappop(heap)
+        sub_boxes = divide_box(box)
+        if len(sub_boxes) == 1:
+            return dist
+
+        for sub_box in sub_boxes:
+            box_score = count_intersection(sub_box, nanobots)
+            heappush(heap, (-box_score, distance_to_origin(sub_box), sub_box))
 
 
 # END OF SOLUTION
