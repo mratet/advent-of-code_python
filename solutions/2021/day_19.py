f77cb7566939fc31e36e30651387e9ee0ba3c164
mratet <mratet.vignat@gmail.com>
2025-08-19 18:46:26 +0200

    AOC 2021 - 19 clean ver

--- solutions/2021/day_19.py
+++ solutions/2021/day_19.py
@@ -5,145 +5,20 @@ import numpy as np
 
 from aocd import get_data
 
-N = 1000
 input = get_data(day=19, year=2021).split("\n\n")
 
-input = """--- scanner 0 ---
-404,-588,-901
-528,-643,409
--838,591,734
-390,-675,-793
--537,-823,-458
--485,-357,347
--345,-311,381
--661,-816,-575
--876,649,763
--618,-824,-621
-553,345,-567
-474,580,667
--447,-329,318
--584,868,-557
-544,-627,-890
-564,392,-477
-455,729,728
--892,524,684
--689,845,-530
-423,-701,434
-7,-33,-71
-630,319,-379
-443,580,662
--789,900,-551
-459,-707,401
+MIN_BEACON = 12
 
---- scanner 1 ---
-686,422,578
-605,423,415
-515,917,-361
--336,658,858
-95,138,22
--476,619,847
--340,-569,-846
-567,-361,727
--460,603,-452
-669,-402,600
-729,430,532
--500,-761,534
--322,571,750
--466,-666,-811
--429,-592,574
--355,545,-477
-703,-491,-529
--328,-685,520
-413,935,-424
--391,539,-444
-586,-435,557
--364,-763,-893
-807,-499,-711
-755,-354,-619
-553,889,-390
 
---- scanner 2 ---
-649,640,665
-682,-795,504
--784,533,-524
--644,584,-595
--588,-843,648
--30,6,44
--674,560,763
-500,723,-460
-609,671,-379
--555,-800,653
--675,-892,-343
-697,-426,-610
-578,704,681
-493,664,-388
--671,-858,530
--667,343,800
-571,-461,-707
--138,-166,112
--889,563,-600
-646,-828,498
-640,759,510
--630,509,768
--681,-892,-333
-673,-379,-804
--742,-814,-386
-577,-820,562
-
---- scanner 3 ---
--589,542,597
-605,-692,669
--500,565,-823
--660,373,557
--458,-679,-417
--488,449,543
--626,468,-788
-338,-750,-386
-528,-832,-391
-562,-778,733
--938,-730,414
-543,643,-506
--524,371,-870
-407,773,750
--104,29,83
-378,-903,-323
--778,-728,485
-426,699,580
--438,-605,-362
--469,-447,-387
-509,732,623
-647,635,-688
--868,-804,481
-614,-800,639
-595,780,-596
-
---- scanner 4 ---
-727,592,562
--293,-554,779
-441,611,-461
--714,465,-776
--743,427,-804
--660,-479,-426
-832,-632,460
-927,-485,-438
-408,393,-506
-466,436,-512
-110,16,151
--258,-428,682
--393,719,612
--211,-452,876
-808,-476,-593
--575,615,604
--485,667,467
--680,325,-822
--627,-443,-432
-872,-547,-609
-833,512,582
-807,604,487
-839,-516,451
-891,-625,532
--652,-548,-490
-30,-46,-14""".split("\n\n")
+def _parse_input(input_blocks):
+    scanners = {}
+    for scanner_id, lines in enumerate(input_blocks):
+        scanner_coords = []
+        for line in lines.splitlines()[1:]:
+            x, y, z = map(int, re.findall(r"(-?\d+)", line))
+            scanner_coords.append((x, y, z))
+        scanners[scanner_id] = np.array(scanner_coords)
+    return scanners
 
 
 def generate_rotation_matrices():
@@ -160,29 +35,99 @@ def generate_rotation_matrices():
     return rotation_matrices
 
 
-rotations = generate_rotation_matrices()
-print(f"Generated {len(rotations)} rotation matrices.")
-scanner = {}
+def compute_distance_between_beacons(M):
+    norms = np.sum(M**2, axis=1)  # shape (50,)
+    dot_products = M @ M.T  # shape (50, 50)
+    return norms[:, None] + norms[None, :] - 2 * dot_products
 
-for scanner_id, lines in enumerate(input):
-    scanner_view = np.zeros((N, N, N), dtype=int)
-    scanner_coords = []
-    for line in lines.splitlines()[1:]:
-        x, y, z = re.findall(r"(-?\d+)", line)
-        scanner_coords.append((int(x), int(y), int(z)))
-    scanner_coords = np.array(scanner_coords)
-    scanner[scanner_id] = [scanner_coords @ R for R in rotations]
+
+def find_matching(A, B):
+    # A -> (n, n), B -> (m, m)
+    # A[:, None, :, None] -> (n, 1, n, 1)
+    # B[None, :, None, :] -> (1, m, 1, m)
+    # equality_matrix[i, j, k, l] = (A[i, k] == B[j, l])
+    equality_matrix = A[:, None, :, None] == B[None, :, None, :]
+
+    diag_n = np.arange(A.shape[0])
+    diag_m = np.arange(B.shape[0])
+    equality_matrix[diag_n, :, diag_n, :] = False
+    equality_matrix[:, diag_m, :, diag_m] = False
+
+    intersection_counts = np.sum(np.any(equality_matrix, axis=3), axis=2)
+    return np.argwhere(intersection_counts >= MIN_BEACON - 1)
+
+
+def solve(scanners_relative):
+    ROTATIONS = generate_rotation_matrices()
+    IDENTITY_MATRIX = np.identity(3, dtype=int)
+    IDENTITY_ROT_ID = next(
+        i for i, R in enumerate(ROTATIONS) if np.array_equal(R, IDENTITY_MATRIX)
+    )
+
+    scanners_absolute = {0: ([0, 0, 0], IDENTITY_ROT_ID)}  # 3 is the id for identity
+    distance_matrices = {
+        sid: compute_distance_between_beacons(s_data)
+        for sid, s_data in scanners_relative.items()
+    }
+
+    queue = [0]
+    processed_scanners = []
+    while queue:
+        base_scanner_id = queue.pop()
+        if base_scanner_id in processed_scanners:
+            continue
+        processed_scanners.append(base_scanner_id)
+
+        base_scanner = scanners_relative[base_scanner_id]
+        base_dist_matrix = distance_matrices[base_scanner_id]
+        base_coords, base_rot_id = scanners_absolute[base_scanner_id]
+        base_R = ROTATIONS[base_rot_id]
+
+        for scanner_id, scanner in scanners_relative.items():
+            if scanner_id == base_scanner_id:
+                continue
+            matching_pairs = find_matching(
+                base_dist_matrix, distance_matrices[scanner_id]
+            )
+            if len(matching_pairs) >= MIN_BEACON:
+                base_idx, idx = np.array(matching_pairs).T
+                s0, s1 = base_scanner[base_idx], scanner[idx]
+                for idr, R in enumerate(ROTATIONS):
+                    uniques = np.unique(s0 @ base_R - s1 @ R, axis=0)
+                    if len(uniques) == 1:
+                        ref_coords = uniques[0] + np.array(base_coords)
+                        scanners_absolute[scanner_id] = (ref_coords.tolist(), idr)
+                        queue.append(scanner_id)
+    return scanners_absolute
 
 
 # WRITE YOUR SOLUTION HERE
 def part_1(lines):
-    return
+    ROTATIONS = generate_rotation_matrices()
+    scanners_relative = _parse_input(lines)
+    scanners_absolute = solve(scanners_relative)
+
+    B = set()
+    for sid, (scoord, srot) in scanners_absolute.items():
+        R = ROTATIONS[srot]
+        for beacon in scanners_relative[sid]:
+            beacon_coords = scoord + beacon @ R
+            B.add(tuple(beacon_coords.tolist()))
+
+    return len(B)
 
 
 def part_2(lines):
-    return
+    scanners_relative = _parse_input(lines)
+    scanners_absolute = solve(scanners_relative)
+
+    SCOORD = [scoord for (scoord, _) in scanners_absolute.values()]
+    return max(
+        (abs(s0x - s1x) + abs(s0y - s1y) + abs(s0z - s1z))
+        for (s0x, s0y, s0z), (s1x, s1y, s1z) in product(SCOORD, repeat=2)
+    )
 
 
 # END OF SOLUTION
-# print(f'My answer is {part_1(input)}')
-# print(f'My answer is {part_2(input)}')
+print(f"My answer is {part_1(input)}")
+print(f"My answer is {part_2(input)}")
