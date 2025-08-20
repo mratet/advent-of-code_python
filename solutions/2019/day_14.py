5d2f0f9aa40f5b01e2aaa00713a15a90702ea521
mratet <mratet.vignat@gmail.com>
2025-08-20 09:14:01 +0200

    AOC 2019 - 14 clean ver

--- solutions/2019/day_14.py
+++ solutions/2019/day_14.py
@@ -1,59 +1,79 @@
 from collections import defaultdict
 
+from math import ceil
 from aocd import get_data
 
 aoc_input = get_data(day=14, year=2019).splitlines()
-from math import ceil
 
-aoc_input = """9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL""".splitlines()
+TRILLION = 1000000000000
+
+
+def _parse_input(lines):
+    reactions = {}
+    for line in lines:
+        inputs_str, output_str = line.split(" => ")
+        output_amount, output_chem = output_str.split()
+        ingredients = []
+        for item in inputs_str.split(", "):
+            amount, chem = item.split()
+            ingredients.append((chem, int(amount)))
+        reactions[output_chem] = (int(output_amount), ingredients)
+    return reactions
+
+
+# Topological sort
+def get_reaction_order(reactions):
+    order = []
+    visited = set()
+
+    def visit(chemical):
+        if chemical == "ORE":
+            return
+        if chemical in visited:
+            return
+        visited.add(chemical)
+        _, ingredients = reactions[chemical]
+        for ingredient_name, _ in ingredients:
+            visit(ingredient_name)
+        order.append(chemical)
+
+    visit("FUEL")
+    return order[::-1]
+
+
+def solve(reaction_order, reactions, fuel_needed=1):
+    chemicals_needed = defaultdict(int)
+    chemicals_needed["FUEL"] = fuel_needed
+    for chemical in reaction_order:
+        output_amount, ingredients = reactions[chemical]
+        reaction_count = ceil(chemicals_needed[chemical] / output_amount)
+        for chem, amount in ingredients:
+            chemicals_needed[chem] += amount * reaction_count
+    return chemicals_needed["ORE"]
 
 
 # WRITE YOUR SOLUTION HERE
 def part_1(lines):
-    reactions = defaultdict(list)
-    chemicals = set()
-    out_mul = {}
-    for line in lines:
-        in_chem, out_chem = line.split(" => ")
-        w_out, out_comp = out_chem.split(" ")
-        out_mul[out_comp] = int(w_out)
-        for chem in in_chem.split(", "):
-            w, comp = chem.split(" ")
-            reactions[out_comp].append((comp, int(w)))
-            chemicals.add(comp)
-
-    out_mul["ORE"] = 1
-
-    def find_decomposition(chem, quantity_wanted):
-        if quantity_wanted == 0:
-            return {}
-        cnt = ceil(quantity_wanted / out_mul[chem])
-        if chem == "ORE":
-            return {"ORE": quantity_wanted}
-        decomp = defaultdict(int)
-        for comp, w in reactions[chem]:
-            d = find_decomposition(comp, w * cnt)
-            for c, c_w in d.items():
-                decomp[c] += c_w
-        return decomp
-
-    print(find_decomposition("FUEL", 1))
-
-    return
+    reactions = _parse_input(lines)
+    reaction_order = get_reaction_order(reactions)
+    return solve(reaction_order, reactions)
 
 
 def part_2(lines):
-    return
+    reactions = _parse_input(lines)
+    reaction_order = get_reaction_order(reactions)
+
+    l, r = 1, TRILLION
+    while l <= r:
+        fuel = (l + r) // 2
+        ore_needed = solve(reaction_order, reactions, fuel)
+        if ore_needed < TRILLION:
+            l = fuel + 1
+        else:
+            r = fuel - 1
+    return r
 
 
 # END OF SOLUTION
 print(f"My answer is {part_1(aoc_input)}")
-# submit(part_1(aoc_input), part="a", day=14, year=2019)
 print(f"My answer is {part_2(aoc_input)}")
-# submit(part_2(aoc_input), part="b", day=14, year=2019)
