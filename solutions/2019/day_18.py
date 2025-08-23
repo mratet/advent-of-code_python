dbd84406e291bc83860dc5f16790b82ac37715d1
mratet <mratet.vignat@gmail.com>
2025-08-23 09:05:02 +0200

    AOC 2019 - 18 clean ver

--- solutions/2019/day_18.py
+++ solutions/2019/day_18.py
@@ -6,11 +6,13 @@ import math
 from aocd import get_data
 
 DIRS = ((-1, 0), (1, 0), (0, -1), (0, 1))
+START_KEYS = "0123@"
+DIALS_ENTRANCE = {"0": (-1, -1), "1": (1, -1), "2": (-1, 1), "3": (1, 1)}
 
 aoc_input = get_data(day=18, year=2019).splitlines()
 
 
-def parse_input(lines):
+def parse_input(lines, part="part_1"):
     entrance, dots, doors, keys = None, [], {}, {}
     for y in range(len(lines)):
         for x in range(len(lines[0])):
@@ -23,7 +25,15 @@ def parse_input(lines):
                 doors[symb] = (x, y)
             else:
                 keys[symb] = (x, y)
-    return dots, doors, keys
+
+    if part == "part_2":
+        x, y = keys.pop("@")
+        for dx, dy in DIRS:
+            dots.remove((x + dx, y + dy))
+        for entrance_key, (dx, dy) in DIALS_ENTRANCE.items():
+            keys[entrance_key] = (x + dx, y + dy)
+
+    return entrance, dots, doors, keys
 
 
 def build_graph(start, dots, keys, doors):
@@ -33,7 +43,9 @@ def build_graph(start, dots, keys, doors):
     queue = deque([(start, 0, 0)])
     visited = {start}
 
-    keys_dist = {} if reverse_keys[start] == "@" else {reverse_keys[start]: (0, 0)}
+    keys_dist = (
+        {} if reverse_keys[start] in START_KEYS else {reverse_keys[start]: (0, 0)}
+    )
     while queue:
         (x, y), doors_seen, cnt = queue.popleft()
         for dx, dy in DIRS:
@@ -43,7 +55,7 @@ def build_graph(start, dots, keys, doors):
             visited.add(neighbor)
 
             if neighbor in reverse_keys:
-                if (key := reverse_keys[neighbor]) != "@":
+                if (key := reverse_keys[neighbor]) not in START_KEYS:
                     keys_dist[key] = (doors_seen, cnt + 1)
                 queue.append((neighbor, doors_seen, cnt + 1))
             elif neighbor in reverse_doors:
@@ -62,25 +74,29 @@ def build_graph(start, dots, keys, doors):
     return keys_dist
 
 
-def dijkstra(graph):
+def dijkstra(graph, start_keys):
     dist = defaultdict(lambda: math.inf)
-    heap = [(0, ("@", 0))]
+    heap = [(0, (start_keys, 0))]
 
     while heap:
-        dist_node, (current_key, owned_key) = heappop(heap)
-        if owned_key == (1 << (len(graph) - 1)) - 1:
+        dist_node, (current_keys, owned_key) = heappop(heap)
+        if owned_key == (1 << (len(graph) - len(current_keys))) - 1:
             return dist_node
 
         reachable_keys = [
-            (next_key, key_dist)
+            (dial_idx, next_key, key_dist)
+            for dial_idx, current_key in enumerate(current_keys)
             for next_key, (doors_needed, key_dist) in graph[current_key].items()
             if (
                 (doors_needed & owned_key) == doors_needed
                 and ((owned_key & (1 << ascii_lowercase.index(next_key))) == 0)
             )
         ]
-        for next_key, key_dist in reachable_keys:
-            neighbor = next_key, owned_key + (1 << (ascii_lowercase.index(next_key)))
+        for dial_idx, next_key, key_dist in reachable_keys:
+            neighbor = (
+                current_keys[:dial_idx] + next_key + current_keys[dial_idx + 1 :],
+                owned_key + (1 << (ascii_lowercase.index(next_key))),
+            )
             dist_neighbor = dist_node + key_dist
             if dist_neighbor < dist[neighbor]:
                 dist[neighbor] = dist_neighbor
@@ -90,17 +106,17 @@ def dijkstra(graph):
 
 # WRITE YOUR SOLUTION HERE
 def part_1(lines):
-    dots, doors, keys = parse_input(lines)
+    _, dots, doors, keys = parse_input(lines)
     graph = {letter: build_graph(keys[letter], dots, keys, doors) for letter in keys}
-    return dijkstra(graph)
+    return dijkstra(graph, "@")
 
 
 def part_2(lines):
-    return
+    entrance, dots, doors, keys = parse_input(lines, "part_2")
+    graph = {letter: build_graph(keys[letter], dots, keys, doors) for letter in keys}
+    return dijkstra(graph, "0123")
 
 
 # END OF SOLUTION
 print(f"My answer is {part_1(aoc_input)}")
-# submit(part_1(aoc_input), part="a", day=18, year=2019)
 print(f"My answer is {part_2(aoc_input)}")
-# submit(part_2(aoc_input), part="b", day=18, year=2019)
