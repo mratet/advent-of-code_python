8c129b67343e60dc7029b0d781d6ad4b5643e0cc
mratet <mratet.vignat@gmail.com>
2025-08-23 08:31:23 +0200

    AOC 2019 - 18 part 1 clean ver

--- solutions/2019/day_18.py
+++ solutions/2019/day_18.py
@@ -1,30 +1,98 @@
+from collections import deque, defaultdict
+from string import ascii_lowercase, ascii_uppercase
+from heapq import heappush, heappop
+import math
+
 from aocd import get_data
+
+DIRS = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 aoc_input = get_data(day=18, year=2019).splitlines()
 
 
-# WRITE YOUR SOLUTION HERE
 def parse_input(lines):
-    dots, doors, keys = [], {}, {}
-    for x in range(len(lines)):
-        for y in range(len(lines)):
-            symb = lines[x][y]
-            if symb == "#":
+    entrance, dots, doors, keys = None, [], {}, {}
+    for y in range(len(lines)):
+        for x in range(len(lines[0])):
+            symb = lines[y][x]
+            if symb in "#":
                 continue
-            elif symb == "@":
-                entrance = (x, y)
             elif symb == ".":
                 dots.append((x, y))
-            elif symb.islower():
-                keys[symb] = (x, y)
-            else:
+            elif symb.isupper():
                 doors[symb] = (x, y)
-    return entrance, dots, doors, keys
+            else:
+                keys[symb] = (x, y)
+    return dots, doors, keys
 
 
+def build_graph(start, dots, keys, doors):
+    reverse_keys = {v: k for k, v in keys.items()}
+    reverse_doors = {v: k for k, v in doors.items()}
+
+    queue = deque([(start, 0, 0)])
+    visited = {start}
+
+    keys_dist = {} if reverse_keys[start] == "@" else {reverse_keys[start]: (0, 0)}
+    while queue:
+        (x, y), doors_seen, cnt = queue.popleft()
+        for dx, dy in DIRS:
+            neighbor = x + dx, y + dy
+            if neighbor in visited:
+                continue
+            visited.add(neighbor)
+
+            if neighbor in reverse_keys:
+                if (key := reverse_keys[neighbor]) != "@":
+                    keys_dist[key] = (doors_seen, cnt + 1)
+                queue.append((neighbor, doors_seen, cnt + 1))
+            elif neighbor in reverse_doors:
+                queue.append(
+                    (
+                        neighbor,
+                        (
+                            doors_seen
+                            + (1 << ascii_uppercase.index(reverse_doors[neighbor]))
+                        ),
+                        cnt + 1,
+                    )
+                )
+            elif neighbor in dots:
+                queue.append((neighbor, doors_seen, cnt + 1))
+    return keys_dist
+
+
+def dijkstra(graph):
+    dist = defaultdict(lambda: math.inf)
+    heap = [(0, ("@", 0))]
+
+    while heap:
+        dist_node, (current_key, owned_key) = heappop(heap)
+        if owned_key == (1 << (len(graph) - 1)) - 1:
+            return dist_node
+
+        reachable_keys = [
+            (next_key, key_dist)
+            for next_key, (doors_needed, key_dist) in graph[current_key].items()
+            if (
+                (doors_needed & owned_key) == doors_needed
+                and ((owned_key & (1 << ascii_lowercase.index(next_key))) == 0)
+            )
+        ]
+        for next_key, key_dist in reachable_keys:
+            neighbor = next_key, owned_key + (1 << (ascii_lowercase.index(next_key)))
+            dist_neighbor = dist_node + key_dist
+            if dist_neighbor < dist[neighbor]:
+                dist[neighbor] = dist_neighbor
+                heappush(heap, (dist_neighbor, neighbor))
+    return dist
+
+
+# WRITE YOUR SOLUTION HERE
 def part_1(lines):
-    start, dots, doors, keys = parse_input(lines)
-    return
+    dots, doors, keys = parse_input(lines)
+    graph = {letter: build_graph(keys[letter], dots, keys, doors) for letter in keys}
+    return dijkstra(graph)
 
 
 def part_2(lines):
