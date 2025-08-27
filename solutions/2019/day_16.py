9bc48fcc5f99f9b88c75099f9d383a8e5dc63346
mratet <mratet.vignat@gmail.com>
2025-08-27 07:58:51 +0200

    AOC 2019 - 16 clean ver

--- solutions/2019/day_16.py
+++ solutions/2019/day_16.py
@@ -1,7 +1,10 @@
 from aocd import get_data
+import numpy as np
 
 aoc_input = get_data(day=16, year=2019)
-import numpy as np
+
+NUM_REPETITIONS = 10_000
+N_PHASE = 100
 
 
 # WRITE YOUR SOLUTION HERE
@@ -18,17 +21,24 @@ def build_fft(N):
 def part_1(lines):
     signal = np.array([int(s) for s in str(lines)])
     fft = build_fft(len(signal))
-    n_phase = 100
-    for _ in range(n_phase):
+    for _ in range(N_PHASE):
         signal = abs(signal @ fft.T) % 10
     return "".join([str(n) for n in signal[:8]])
 
 
 def part_2(lines):
-    for i in range(5, 25, 5):
-        print(build_fft(i))
-        print()
-    return
+    offset = int(lines[:7])
+    total_signal_length = len(lines) * NUM_REPETITIONS
+    # [...start####][####][####]...
+    start, repeat = offset % len(lines), (total_signal_length - offset) // len(lines)
+    signal = np.array([int(s) for s in str(lines)])
+    repeated_signal = np.tile(signal, repeat + 1)
+
+    cumsum_from_right = lambda x: x[::-1].cumsum()[::-1]
+    # In low frequencies (i.e right part of the fft), we're only averaging values
+    for _ in range(N_PHASE):
+        repeated_signal = abs(cumsum_from_right(repeated_signal)) % 10
+    return "".join([str(d) for d in repeated_signal[start : start + 8]])
 
 
 # END OF SOLUTION
